b6cbfeb5b5bed7e94ecaa8797a8817f399942509
SeungKu_Kang <kangsg26@naver.com>
2023-07-13 16:44:01 +0900

    * '문자열 겹쳐쓰기' 문제 성능비교
    - 코드추가
    - README.md파일 수정

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,5 +1,9 @@
 class Solution {
-    public String solution(String my_string, String overwrite_string, int s) {
+    public String solution1(String my_string, String overwrite_string, int s) {
+        return my_string.substring(0, s) + overwrite_string + my_string.substring(s + overwrite_string.length(), my_string.length());
+    }
+
+    public String solution2(String my_string, String overwrite_string, int s) {
         String before = my_string.substring(0, s);
         String after = my_string.substring(s + overwrite_string.length());
         return before + overwrite_string + after;
