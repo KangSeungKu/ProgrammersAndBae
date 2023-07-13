f1a988b599ee2f8c08c0b2594b81ada04d078e7e
SeungKu_Kang <kangsg26@naver.com>
2023-07-13 20:27:53 +0900

    * '두 수의 연산값 비교하기'관련 코드 개선
    - 코드개선
    - README.md파일 수정

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -1,5 +1,9 @@
 class Solution {
-    public int solution(int a, int b) {
+    public int solution1(int a, int b) {
+        return Math.max(Integer.parseInt(a + "" + b), 2 * a * b);
+    }
+
+    public int solution2(int a, int b) {
         return Math.max(Integer.parseInt(String.valueOf(a)+String.valueOf(b)),2*a*b);
     }
 }
