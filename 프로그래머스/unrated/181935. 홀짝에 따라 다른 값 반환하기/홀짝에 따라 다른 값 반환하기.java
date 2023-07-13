5cf4819107eb7c40bd0f02f97de9e619ed821698
SeungKu_Kang <kangsg26@naver.com>
2023-07-14 00:53:59 +0900

    * '홀짝에 따라 다른 값 반환하기' 다른 풀이 추가
    - solution2 추가
    - README.md파일 수정

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -1,7 +1,19 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public int solution(int n) {
+	public int solution1(int n) {
+        int answer = 0;
+        int i = 0;
+        int length = n + 1;
+        
+        for(i = 1; i < length; i++) {
+        	answer += n % 2 == 0 ? i%2 == 0 ? Math.pow(i, 2) : 0 : i%2 == 0 ? 0 : i; 
+        }
+        
+        return answer;
+    }
+
+    public int solution2(int n) {
         int result = 0;
 		
 		if(n % 2 == 0) {
