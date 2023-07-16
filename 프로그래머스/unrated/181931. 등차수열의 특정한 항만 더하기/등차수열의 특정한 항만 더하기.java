662db2e30cfa67b6a42a52a6e7e07810e7aa26ca
SeungKu_Kang <kangsg26@naver.com>
2023-07-16 14:55:59 +0900

    * '등차수열의 특정한 항만 더하기'문제 성능비교
    - 일반 for문과 IntStream의 처리속도 비교
    - README.md 파일수정

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -1,7 +1,19 @@
 import java.util.stream.IntStream;
 
 class Solution {
-    public int solution(int a, int d, boolean[] included) {
+    public int solution1(int a, int d, boolean[] included) {
+        int answer = 0;
+        int seq = a;
+        
+        for(boolean inc : included) {
+        	answer += inc ? seq : 0;
+        	seq += d;
+        }
+        
+        return answer;
+    }
+    
+    public int solution2(int a, int d, boolean[] included) {
         return IntStream.range(0, included.length).map(x -> included[x] ? a + d * x : 0).sum();
     }
 }
