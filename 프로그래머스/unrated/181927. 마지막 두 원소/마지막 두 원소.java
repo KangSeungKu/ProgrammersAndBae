2047fbd575d5cc9d51a50b0f88175c36dd0b8401
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-07-17 23:01:21 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = Arrays.copyOf(num_list, num_list.length + 1);
+    	answer[num_list.length] = num_list[num_list.length - 1] > num_list[num_list.length - 2] ? num_list[num_list.length - 1] - num_list[num_list.length - 2] : 2 * num_list[num_list.length - 1];
+        
+        return answer;
+    }
+}
