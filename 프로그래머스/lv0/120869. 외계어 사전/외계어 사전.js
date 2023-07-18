c53f4d8ce383e43aa6fd686f7ef23c060050c990
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-07-19 01:00:55 +0900

    [level 0] Title: 외계어 사전, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,1 +1,1 @@
-const solution = (spell, dic) => dic.filter(str => spell.filter(ch => str.includes(ch)).length === spell.length).length ? 1 : 2;
+const solution = (spell, dic) => dic.filter(str => spell.every(ch => str.includes(ch))).length ? 1 : 2;
