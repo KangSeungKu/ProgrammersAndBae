945943354af47d6e3e3fbc3ac0e179038058cb9f
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-07-18 20:02:50 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,16 +1,9 @@
-const solution = lines => {
-    const mapMergedLine = new Map();
-    let line = 0;
+function solution(lines) {
+    let line = new Array(200).fill(0);
 
-    for([start, end] of lines) {
-        for(let i = start; i < end; i++) {
-            mapMergedLine.has(i) ? mapMergedLine.set(i, mapMergedLine.get(i) + 1) : mapMergedLine.set(i, 0);
-        }
-    }
+    lines.forEach(([a, b]) => {
+        for(; a < b; a++) line[a+100]++;
+    });
 
-    for([_, value] of mapMergedLine) {
-        line += value > 0 ? 1 : 0;
-    }
-
-    return line;
+    return line.reduce((a, c) =>  c > 1 ? a + 1 : a, 0)
 }
