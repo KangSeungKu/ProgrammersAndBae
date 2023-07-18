d2b51e495594abadeaa1c1a7f414fdaba9a65512
SeungKu_Kang <kangsg26@naver.com>
2023-07-18 20:05:36 +0900

    * [Level 0] 겹치는 선분의 길이 비교분석
    - Map이 아닌 단순배열을 사용하는 코드추가
    - README.md 파일수정

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,4 +1,21 @@
-function solution(lines) {
+const solution = lines => {
+    const mapMergedLine = new Map();
+    let line = 0;
+
+    for([start, end] of lines) {
+        for(let i = start; i < end; i++) {
+            mapMergedLine.has(i) ? mapMergedLine.set(i, mapMergedLine.get(i) + 1) : mapMergedLine.set(i, 0);
+        }
+    }
+
+    for([_, value] of mapMergedLine) {
+        line += value > 0 ? 1 : 0;
+    }
+
+    return line;
+}
+
+function solution2(lines) {
     let line = new Array(200).fill(0);
 
     lines.forEach(([a, b]) => {
