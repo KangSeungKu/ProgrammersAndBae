a924f8ec47eb2ffcbc349246f57a2f5ac8d44539
SeungKu_Kang <kangsg26@naver.com>
2023-07-19 01:05:47 +0900

    * [Level 0] '외계어 사전' 코드개선
    - 두번째 반복문에서 filter를 every로 변경
        -> 극적인 효과는 보지 못 함.
    - README.md 파일수정

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,1 +1,2 @@
-const solution = (spell, dic) => dic.filter(str => spell.every(ch => str.includes(ch))).length ? 1 : 2;
+const solution1 = (spell, dic) => dic.filter(str => spell.filter(ch => str.includes(ch)).length === spell.length).length ? 1 : 2;
+const solution2 = (spell, dic) => dic.filter(str => spell.every(ch => str.includes(ch))).length ? 1 : 2;
