3b24f08f6893caf3868cbfa074c8e104a17938c6
SeungKu_Kang <kangsg26@naver.com>
2023-07-23 20:01:06 +0900

    * [Level 0] '다항식 더하기'문제 요약
    - '다른 사람의 풀이'를 참고한 코드추가
    - README.md파일 수정

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,4 +1,26 @@
-const solution = polynomial => {
+// 기존방식
+const solution1 = polynomial => {
+    const polyn = polynomial.replaceAll(" + ", " ").split(" ");
+    let res = "";
+    let x = 0;
+    let con = 0;
+
+    polyn.forEach(n => {
+        if(n.includes("x")) {
+            x += n.length === 1 ? 1 : Number(n.replace("x", ""));
+        } else {
+            con += Number(n);
+        }
+    });
+
+    res = x === 1 ? "x" : x > 1 ? x + "x" : "";
+    res += con === 0 ? "" : x > 0 ? " + " + con : con;
+
+    return res;
+}
+
+// '다른 사람의 풀이'를 참고한 코드
+const solution2 = polynomial => {
     const polyn = polynomial.split(" + ");
     let res = [];
     let x = 0;
