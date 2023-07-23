c683aa87b2165c4bfd9bf063d81eade7daddb4a7
SeungKu_Kang <kangsg26@naver.com>
2023-07-24 01:52:48 +0900

    * [Level 0] '최댓값 만들기 (2)' 요약
    - '다른 사람의 풀이' 코드추가
    - README.md 파일수정

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,4 +1,18 @@
-const solution = numbers => {
+const solution1 = numbers => {
+    let res = numbers[0] * numbers[1];
+    let num = numbers.pop();
+
+    while(numbers.length) {
+        numbers.forEach(n => res = Math.max(res, num * n));
+        num = numbers.pop();
+    }
+
+    return res;
+}
+
+// '다른 사람의 풀이' 중 괜찮다고 생각되는 코드
+// 정렬을 이용하여 음수를 고려한 양끝의 곱 중 큰 값을 비교하는 방식
+const solution2 = numbers => {
     numbers.sort((a,b) => a - b);
     return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
 }
