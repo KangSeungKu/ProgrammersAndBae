429bb9cfa372a32b90b662520c705603bfc0ad66
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-07-24 01:50:09 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,11 +1,4 @@
 const solution = numbers => {
-    let res = numbers[0] * numbers[1];
-    let num = numbers.pop();
-
-    while(numbers.length) {
-        numbers.forEach(n => res = Math.max(res, num * n));
-        num = numbers.pop();
-    }
-
-    return res;
+    numbers.sort((a,b) => a - b);
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
 }
