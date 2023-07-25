f4a8e1852c1002d290b019444eff73f94fd99a8b
SeungKu_Kang <kangsg26@naver.com>
2023-07-25 13:55:47 +0900

    * [Level 0] '모음 제거' 문제 비교
    - '|'로 사용하였을 경우와 '[]'로 그룹으로 사용하였을 경우의 차이
    - README.md파일 수정

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,1 +1,2 @@
-const solution = my_string => my_string.replace(/[aeiou]/g,'');
+const solution1 = my_string => my_string.replace(/a|e|i|o|u/g,'');
+const solution2 = my_string => my_string.replace(/[aeiou]/g,'');
