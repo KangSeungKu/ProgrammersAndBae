6cbbc89131258594ca71d24d397df5d93567fdbb
SeungKu_Kang <kangsg26@naver.com>
2023-07-26 02:20:47 +0900

    * [Level 0] '팩토리얼' 문제 요약
    - '다른 사람의 풀이' 추가
    - README.md 파일 수정

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,3 +1,15 @@
+// 기존에 풀었던 방식
+const solution1 = n => {
+    let answer = 0;
+
+    while(getPactorial(answer) <= n) answer++;
+
+    return --answer;
+}
+
+const getPactorial = n => n > 1 ? n * getPactorial(--n) : 1;
+
+// '다른 사람의 풀이' 중 괜찮다고 생각되는 코드
 const solution = n => {
     let answer = 1;
     let ftrl = 1;
