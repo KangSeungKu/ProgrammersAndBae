9de7f6dd4781dce6107065dd12ed5f6cb708122b
SeungKu_Kang <kangsg26@naver.com>
2023-07-27 21:23:31 +0900

    * [Level 0] '공 던지기' 문제요약
    - '다른 사람의 풀이' 추가
    - README.md파일 수정

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,1 +1,14 @@
-const solution = (numbers, k) => numbers[((k - 1) * 2) % numbers.length];
+// 처음 풀었을 때, 방식
+const solution1 = (numbers, k) => {
+    let flag = 0;
+
+    while(flag < k - 1) {
+        numbers = [...numbers.slice(2), ...numbers.slice(0, 2) ];
+        flag++;
+    }
+
+    return numbers[0];
+}
+
+// 연산을 통한 결과
+const solution2 = (numbers, k) => numbers[((k - 1) * 2) % numbers.length];
