da181bc9cf2b7009acfc02e9b01f65b7ebc788d9
SeungKu_Kang <kangsg26@naver.com>
2023-07-28 14:08:00 +0900

    * [Level 0] '진료 순서 정하기' 문제 요약
    - 배열의 indexOf로 해결하는 코드추가
    - README.md파일수정

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,16 @@
-const solution = emergency => {
+// Map객체를 사용했을 경우
+const solution1 = emergency => {
+    const mapRank = new Map();
+
+    emergency.slice().sort((a, b) => b - a).forEach((score, rank) => {
+        mapRank.set(score, rank + 1);
+    })
+
+    return emergency.map(score => mapRank.get(score));
+}
+
+// 배열의 indexOf를 사용했을 경우
+const solution2 = emergency => {
     const rankingArr = emergency.slice().sort((a, b) => b - a);
     return emergency.map(score => rankingArr.indexOf(score) + 1);
 }
