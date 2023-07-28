f0432db099ccd25a2598a2087731fe290ff742e1
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-07-28 14:57:20 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,14 @@
+const solution = age => String(age).split("").map(n => alpha[n]).join("");
+
+const alpha = {
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h",
+    8: "i",
+    9: "j",
+}
