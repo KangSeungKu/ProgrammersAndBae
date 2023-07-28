4ed984985a6965859f2c5348893e3249cb148cf8
SeungKu_Kang <kangsg26@naver.com>
2023-07-28 15:48:26 +0900

    * [Level 0] '각도기' 문제 요약
    - '다른 사람의 풀이' 중 신박해 보이는 코드 추가
    - README.md 파일 수정

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,1 +1,18 @@
-const solution = angle => [0, 90, 91, 180].filter(x => angle >= x).length;
+const solution1 = angle => {
+    let res = 0;
+
+    if(angle === 180) {
+        res = 4;
+    } else if(angle > 90) {
+        res = 3;
+    } else if(angle === 90) {
+        res = 2;
+    } else {
+        res = 1;
+    }
+
+    return res;
+}
+
+// '다른 사람의 풀이' 중 신박해서 추가.
+const solution2 = angle => [0, 90, 91, 180].filter(x => angle >= x).length;
