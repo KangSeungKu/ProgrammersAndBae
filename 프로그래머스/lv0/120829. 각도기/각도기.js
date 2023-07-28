20241e72ca6c5aa6d4a8e935d20323f0f3446940
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-07-28 15:45:24 +0900

    [level 0] Title: 각도기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,15 +1,1 @@
-const solution = angle => {
-    let res = 0;
-
-    if(angle === 180) {
-        res = 4;
-    } else if(angle > 90) {
-        res = 3;
-    } else if(angle === 90) {
-        res = 2;
-    } else {
-        res = 1;
-    }
-
-    return res;
-}
+const solution = angle => [0, 90, 91, 180].filter(x => angle >= x).length;
