b7c56a6407fd02acf526e6a21bd7c4b5eed47b13
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-07-30 21:38:10 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,18 +1,9 @@
 const solution = (numer1, denom1, numer2, denom2) => {
     const numer = numer1 * denom2 + numer2 * denom1;
     const denom = denom1 * denom2;
-    const lcm = getGCD(numer, denom);
+    const lcm = fnGCD(numer, denom);
 
     return [numer/lcm, denom/lcm];
 }
 
-const getGCD = (num1, num2) => {
-  
-    while(num2 > 0){
-        let r = num1 % num2;
-        num1 = num2;
-        num2 = r;
-    } 
-
-    return num1;
-}
+const fnGCD = (a, b) => (a % b) ? fnGCD(b, a % b) : b;
