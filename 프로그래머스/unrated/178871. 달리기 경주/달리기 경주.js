f1208a43a10e8a5dbc8f68182e47fa69846631cd
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-08-01 06:40:28 +0900

    [unrated] Title: 달리기 경주, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,26 +1,17 @@
 const solution = (players, callings) => {
-    const playersMap = new Map();   // { 0 => 'mumu', 1 => 'soe', 2 => 'kai', 3 => 'poe', 4 => 'mine' }
-    const playersRankMap = new Map();   // { 'mumu' => 0, 'soe' => 1, 'poe' => 3, 'kai' => 2, 'mine' => 4 }
+    const rankObj = {};
 
-    // Map initialization
-    for(let i = 0; i < players.length; i++) {
-        playersMap.set(i, players[i]);
-        playersRankMap.set(players[i], i);
-    }
+    players.forEach((player, rank) => rankObj[player] = rank);
+    callings.forEach(call => {
+        let idx = rankObj[call];
+        let tmpPlayer = players[idx-1];
 
-    callings.forEach(player => {
-        // Swap
-        let idx = playersRankMap.get(player); // 3
-        let tmpPlayer = playersMap.get(idx - 1); // "poe"
+        players[idx-1] = call;
+        rankObj[call]--;
 
-        // Players reconstruct
-        playersMap.set(idx - 1, player);  // 2: "poe" -> 2: "kai"
-        playersMap.set(idx, tmpPlayer);  // 3: "kai" -> 3: "poe"
-
-        // Rank reconstruct
-        playersRankMap.set(player, idx - 1); // "poe": 2 -> "kai": 2
-        playersRankMap.set(tmpPlayer, idx); // "kai": 3 -> "poe": 3
+        players[idx] = tmpPlayer;
+        rankObj[tmpPlayer]++;
     });
 
-    return [...playersMap.values()];
-};
+    return players;
+}
