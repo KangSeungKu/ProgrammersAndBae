8df189166de2cc5ae3a2e58c48643e49c700331d
SeungKu_Kang <kangsg26@naver.com>
2023-08-01 14:56:25 +0900

    * [Level 1] '추억 점수' 문제 요약
    - '다른 사람의 풀이' 추가
    - README.md파일 수정

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,1 +1,16 @@
-const solution = (name, yearning, photo) => photo.map((ph)=> ph.reduce((acc, cur)=> acc += yearning[name.indexOf(cur)] ?? 0, 0));
+// 처음 풀었을 때 방법
+const solution1 = (name, yearning, photo) => {
+    const memoryScoreObj = {};
+    let answer = 0;
+
+    name.forEach((nm, idx) => memoryScoreObj[nm] = yearning[idx]);
+    answer = photo.map(phArr => phArr.reduce((acc, cur) => acc + (isNaN(memoryScoreObj[cur]) ? 0 : memoryScoreObj[cur]), 0));
+
+    return answer;
+}
+
+// ES6 문법을 활용한 방법 (다른 사람의 풀이)
+// map, reduce함수를 사용한 것은 동일.
+// 별도의 Object를 사용하지 않고 파라미터로 들어온 Array를 그대로 사용.
+// 널 병합 연산자(??)를 이용하여 '그리움 점수'가 없을 때를 처리.
+const solution2 = (name, yearning, photo) => photo.map((ph)=> ph.reduce((acc, cur)=> acc += yearning[name.indexOf(cur)] ?? 0, 0));
