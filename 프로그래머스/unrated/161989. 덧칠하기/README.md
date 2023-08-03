fab8c6b799a941714b40b67ba8dfcff968a0dc59
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-08-04 01:01:27 +0900

    [unrated] Title: 덧칠하기, Time: 163.30 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/README.md
+++ 프로그래머스/unrated/161989. 덧칠하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 덧칠하기 - 161989 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989?language=javascript) 
 
 ### 성능 요약
 
-메모리: 37.4 MB, 시간: 1.47 ms
+메모리: 39.2 MB, 시간: 163.30 ms
 
 ### 구분
 
--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,13 +1,15 @@
 const solution = (n, m, section) => {
+    let tmpSection = section.slice();
     let drawn = 0;
-    let basis = section[0]
 
-    section.forEach(sect => {
-        if(!(basis <= sect && sect < (basis + m))) {
-            drawn++;
-            basis = sect;
+    while(tmpSection.length) {
+        let draw = tmpSection.at(0);
+        drawn++;
+
+        while(tmpSection.at(0) < (draw + m)) {
+            tmpSection.shift();
         }
-    });
+    }
 
-    return ++drawn;
+    return drawn;
 }
