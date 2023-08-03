441bb39a1a48e2daf5e2c45173432efed584dca6
SeungKu_Kang <kangsg26@naver.com>
2023-08-04 01:06:27 +0900

    * [Level 1] '덧칠하기' 문제 요약
    - 코드 추가
    - README.md 파일 수정

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,4 +1,18 @@
-const solution = (n, m, section) => {
+const solution1 = (n, m, section) => {
+    let drawn = 0;
+    let basis = section[0];
+
+    section.forEach(sect => {
+        if(!(basis <= sect && sect < (basis + m))) {
+            drawn++;
+            basis = sect;
+        }
+    });
+
+    return ++drawn;
+}
+
+const solution2 = (n, m, section) => {
     let tmpSection = section.slice();
     let drawn = 0;
 
