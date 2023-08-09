1710553770e2ff487b89e6694063a9f4a11b73ab
SeungKu_Kang <kangsg26@naver.com>
2023-08-10 02:07:18 +0900

    * [Level 1] '개인정보 수집 유효기간' 문제요약
    - '다른 사람의 풀이'를 참고하여 Date객체가 아닌 숫자계산만으로 풀 수 있는 방법 추가
    - README.md 파일 수정

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,4 +1,24 @@
-const solution = (today, terms, privacies) => {
+// 기존에 풀던 방식
+const solution1 = (today, terms, privacies) => {
+    const answer = [];
+    const termMap = new Map(terms.map(term => term.split(" ")));
+    const todayDate = new Date(today);
+
+    privacies.forEach((privacy, idx) => {
+        const [date, termType] = privacy.split(" ");
+        const privacyDate = new Date(date);
+        const termDate = new Date(privacyDate.setMonth(privacyDate.getMonth() + Number(termMap.get(termType))));
+
+        if(todayDate >= termDate) {
+            answer.push(idx + 1);
+        }
+    });
+
+    return answer;
+}
+
+// '다른 사람의 풀이' 중 Date객체가 아닌 숫자계산만으로 처리하는 방식을 적용
+const solution2 = (today, terms, privacies) => {
     const answer = [];
     const termMap = new Map(terms.map(term => term.split(" ")));
     const [year, month, day] = today.split(".").map(Number);
