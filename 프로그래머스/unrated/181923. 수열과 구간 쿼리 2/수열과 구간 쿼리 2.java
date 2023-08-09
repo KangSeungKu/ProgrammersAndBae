12d5a93f66f526f06a28b3ec78ec1c0397ff5838
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-08-09 22:28:48 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 11.34 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,23 +1,13 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
-        int idx = 0;
-        
-        for(int[] query : queries) {
-        	ArrayList<Integer> minList = new ArrayList<Integer>();
-        	
-        	for(int i = query[0]; i <= query[1]; i++) {
-        		if(query[2] < arr[i]) {
-        			minList.add(arr[i]);
-        		}
-        	}
-        	
-        	answer[idx++] = minList.size() == 0 ? -1 : Collections.min(minList);
-        }
-        
-        return answer;
+        int[] answer = {};
+        return IntStream.range(0, queries.length)
+                .map(q -> IntStream.rangeClosed(queries[q][0], queries[q][1])
+                        .map(i -> arr[i])
+                        .filter(i -> i > queries[q][2])
+                        .min().orElse(-1)
+                ).toArray();
     }
 }
