9457b1624eda1bfeae8423a5275f872407a34652
SeungKu_Kang <kangsg26@naver.com>
2023-08-09 22:31:16 +0900

    * [Level 0] '수열과 구간 쿼리2' 문제요약
    - '다른 사람의 풀이' 추가
    - README.md파일 수정

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,7 +1,28 @@
 import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
+    public int[] solution1(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        int idx = 0;
+        
+        for(int[] query : queries) {
+        	ArrayList<Integer> minList = new ArrayList<Integer>();
+        	
+        	for(int i = query[0]; i <= query[1]; i++) {
+        		if(query[2] < arr[i]) {
+        			minList.add(arr[i]);
+        		}
+        	}
+        	
+        	answer[idx++] = minList.size() == 0 ? -1 : Collections.min(minList);
+        }
+        
+        return answer;
+    }
+
+    public int[] solution2(int[] arr, int[][] queries) {
         int[] answer = {};
         return IntStream.range(0, queries.length)
                 .map(q -> IntStream.rangeClosed(queries[q][0], queries[q][1])
