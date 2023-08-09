9778862f55671e19fe28d57f49ae3cd34a6d6adc
SeungKu_Kang <kangsg26@naver.com>
2023-08-10 03:43:25 +0900

    * [Level 1] '크기가 작은 부분문자열' 문제 요약
    - 기존 reduce로 소비되던 반복수를 줄여 개선시도
    - README.md 파일 수정

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,4 +1,18 @@
-const solution = (t, p) => {
+// 기존코드
+const solution1 = (t, p) => {
+    const tArr = [...t];
+    const splicedT = [];
+
+    while(tArr.length >= p.length) {
+        splicedT.push(Number(tArr.slice(0, p.length).join("")));
+        tArr.shift();
+    }
+
+    return splicedT.reduce((acc, cur) => acc += cur <= Number(p) ? 1 : 0, 0);
+}
+
+// reduce를 사용하지 않고 한 번의 반복으로 처리할 수 있도록 개선
+const solution2 = (t, p) => {
     const tArr = [...t];
     const compareNum = Number(p);
     let cnt = 0;
