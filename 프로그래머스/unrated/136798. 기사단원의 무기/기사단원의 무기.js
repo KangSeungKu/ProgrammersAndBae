64ff9076142d627ac847572cd710f447f033a264
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-08-11 21:33:20 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.14 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,10 +1,23 @@
-const solution = (number, limit, power) => Array.from({length: number}, (_, i) => i + 1).map(n => getDivisor(n)).map(n => n > limit ? power : n).reduce((acc, cur) => acc + cur);
-
-const getDivisor = n => {
+const solution = (number, limit, power) => {
     let res = 0;
 
-    for(let i = 1; i * i <= n; i++) {
-        if(i * i === n) res++;
+    for(let i = 1; i <= number; i++) {
+        let divisor = getDivisor(i);
+        
+        if(divisor > limit) divisor = power;
+
+        res += divisor;
+    }
+
+    return res;
+}
+
+const getDivisor = n => {
+    const sqrtNum = Math.sqrt(n);
+    let res = 0;
+
+    for(let i = 1; i <= sqrtNum; i++) {
+        if(i === sqrtNum) res++;
         else if(!(n % i)) res += 2;
         else 0;
     }
