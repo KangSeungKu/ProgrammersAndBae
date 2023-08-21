ae38b539d0f682bd81a8b380211e842d2d718d38
SeungKu_Kang <kangsg26@naver.com>
2023-08-21 17:22:25 +0900

    * [Level 1] '없는 숫자 더하기' 문제 요약
    - '다른 사람의 풀이'를 참고하여 푼 방법 추가
    - README.md 파일 수정

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -4,7 +4,26 @@
  * @param {Array} numbers 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열
  * @returns numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수
  */
-const solution = numbers => {
+const solution1 = numbers => {
+    const MAX_NUMBER = 10;
+
+    let answer = 0;
+    let idx = 0;
+
+    numbers.sort((a, b) => a - b);
+
+    for(let i = 0; i < MAX_NUMBER; i++) {
+        if(numbers.at(idx) !== i) {
+            answer += i;
+        } else {
+            idx++;
+        }
+    }
+
+    return answer;
+}
+
+const solution2 = numbers => {
     const MAX_NUMBER = 10;
 
     const sumOfMaxNum = Array.from([...Array.from({length: MAX_NUMBER}).keys()]).reduce((acc, cur) => acc + cur, 0);
