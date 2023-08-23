57d57f94b0c0d93d78f02da668a6a2ccb72d7d64
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-08-23 22:14:53 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,29 +1,9 @@
-/**
- * 약수의 개수와 덧셈
- * 
- * @param {Number} left 정수
- * @param {Number} right 정수
- * @returns 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수
- */
 const solution = (left, right) => {
     let answer = 0;
 
-    for(let i = left; i <= right; i++) {
-        answer += !(getDivisor(i) % 2) ? i : (-i)
+    for (let i = left; i <= right; i++) {
+        answer += Number.isInteger(Math.sqrt(i)) ? (-i) : i;
     }
 
     return answer;
-}
-
-const getDivisor = n => {
-    const sqrtNum = Math.sqrt(n);
-    let res = 0;
-
-    for(let i = 1; i <= sqrtNum; i++) {
-        if(i === sqrtNum) res++;
-        else if(!(n % i)) res += 2;
-        else 0;
-    }
-
-    return res;
 }
