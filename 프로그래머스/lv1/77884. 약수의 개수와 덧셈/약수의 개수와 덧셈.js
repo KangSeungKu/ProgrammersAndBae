2cee0f1693d83bdac74b25e4f428c5758c4833f6
SeungKu_Kang <kangsg26@naver.com>
2023-08-23 22:17:59 +0900

    * [Level 1] '약수의 개수와 덧셈' 문제 요약
    - '다른 사람의 풀이'를 참고하여 다른 풀이방법 추가
    - README.md 파일 수정

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,4 +1,36 @@
-const solution = (left, right) => {
+/**
+ * 약수의 개수와 덧셈
+ * 
+ * @param {Number} left 정수
+ * @param {Number} right 정수
+ * @returns 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수
+ */
+const solution1 = (left, right) => {
+    let answer = 0;
+
+    for(let i = left; i <= right; i++) {
+        answer += !(getDivisor(i) % 2) ? i : (-i)
+    }
+
+    return answer;
+}
+
+const getDivisor = n => {
+    const sqrtNum = Math.sqrt(n);
+    let res = 0;
+
+    for(let i = 1; i <= sqrtNum; i++) {
+        if(i === sqrtNum) res++;
+        else if(!(n % i)) res += 2;
+        else 0;
+    }
+
+    return res;
+}
+
+// '다른 사람의 풀이'를 참고하여 작성
+// 제곱근이 정수면 약수는 홀수개인 것을 활용
+const solution2 = (left, right) => {
     let answer = 0;
 
     for (let i = left; i <= right; i++) {
