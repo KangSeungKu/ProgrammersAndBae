2402897b9584b5b130cc4c6aa09228acb644570b
SeungKu_Kang <kangsg26@naver.com>
2023-08-27 23:19:35 +0900

    * [Level 1] '내적' 문제 요약
    - 한 줄 코드 방법 추가
        ; 효율은 좋지 않음.
    - README.md 파일 수정

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,1 +1,19 @@
-const solution = (a, b) => a.reduce((acc, cur, idx) => acc + cur * b[idx], 0);
+/**
+ * 내적
+ * 
+ * @param {Array} a 정수 배열
+ * @param {Array} b 정수 배열
+ * @returns a와 b의 내적
+ */
+const solution1 = (a, b) => {
+    let answer = 0;
+
+    for(let i = 0; i < a.length; i++) {
+        answer += a[i] * b[i];
+    }
+
+    return answer;
+}
+
+// 한 줄 코드
+const solution2 = (a, b) => a.reduce((acc, cur, idx) => acc + cur * b[idx], 0);
