d880fdd60cbab932297604c880f0a3d7f091c770
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-08-27 23:13:36 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,16 +1,1 @@
-/**
- * 내적
- * 
- * @param {Array} a 정수 배열
- * @param {Array} b 정수 배열
- * @returns a와 b의 내적
- */
-const solution = (a, b) => {
-    let answer = 0;
-
-    for(let i = 0; i < a.length; i++) {
-        answer += a[i] * b[i];
-    }
-
-    return answer;
-}
+const solution = (a, b) => a.reduce((acc, cur, idx) => acc + cur * b[idx], 0);
