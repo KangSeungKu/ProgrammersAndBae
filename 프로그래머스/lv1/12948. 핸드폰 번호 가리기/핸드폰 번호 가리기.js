66a5b0bda800eb970379cef058713f90257b54a8
SeungKu_Kang <kangsg26@naver.com>
2023-09-09 23:52:32 +0900

    * [Level 1] '핸드폰 번호 가리기' 문제 요약
    - '다른 사람의 풀이' 중 '정규표현식'을 이용한 방법 추가
    - README.md 파일 수정

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -4,4 +4,10 @@
  * @param {String} phone_number 전화번호
  * @returns 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 '*'으로 가린 문자열
  */
-const solution = phone_number => phone_number.replace(/\d(?=\d{4})/g, "*");
+const solution1 = phone_number => {
+    const len = phone_number.length - 4;
+
+    return new Array(len).fill('*').join('') + phone_number.substring(len);
+}
+
+const solution2 = phone_number => phone_number.replace(/\d(?=\d{4})/g, "*");
