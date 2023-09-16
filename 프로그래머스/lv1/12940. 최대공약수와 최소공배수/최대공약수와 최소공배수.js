d709d6fa4ac845664e2e1af2adda5a30bb7fc35d
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-09-16 17:33:41 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -5,18 +5,8 @@
  * @param {Number} m 정수
  * @returns [최대공약수, 최소공배수]
  */
-const solution = (n, m) => [getGCD(n, m), getLCM(n, m)];
-
-const getGCD = (a, b) => (a % b) ? getGCD(b, a % b) : b;
-
-const getLCM = (a, b) =>{
-	let lcm = 1;
-   
-    while(true){
-      if((lcm % a == 0) && (lcm % b == 0)){
-        break;
-      }
-      lcm++;
-    }
-  	return lcm
+const solution = (a, b) =>  {
+    let r;
+    for(var ab= a*b;r = a % b;a = b, b = r){}
+    return [b, ab/b];
 }
