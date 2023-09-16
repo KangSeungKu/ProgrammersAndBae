e0d15d2e5d65283c6aa2bd6c14a434908403eb02
SeungKu_Kang <kangsg26@naver.com>
2023-09-16 17:37:14 +0900

    * [Level 1] '최대공약수와 최소공배수' 문제 요약
    - '최소공배수' 구하는 함수추가
    - '다른 사람의 풀이' 함수추가
    - README.md 파일 수정

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -5,7 +5,28 @@
  * @param {Number} m 정수
  * @returns [최대공약수, 최소공배수]
  */
-const solution = (a, b) =>  {
+const solution1 = (n, m) => [getGCD(n, m), getLCM(n, m)];
+
+const getGCD = (a, b) => (a % b) ? getGCD(b, a % b) : b;
+
+const getLCM = (a, b) =>{
+	let lcm = 1;
+   
+    while(true){
+      if((lcm % a == 0) && (lcm % b == 0)){
+        break;
+      }
+      lcm++;
+    }
+  	return lcm
+}
+
+// 최소공배수 구하는 다른 방법
+const getLCM2 = (a, b) => (a * b) / getGCD(a, b);
+
+// '다른 사람 풀이' 중 괜찮은 방법
+// 단, 가독성이 매우 떨어짐.
+const solution2 = (a, b) =>  {
     let r;
     for(var ab= a*b;r = a % b;a = b, b = r){}
     return [b, ab/b];
