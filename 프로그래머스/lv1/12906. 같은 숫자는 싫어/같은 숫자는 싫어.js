32fc0ddab06194c0bc90a1f62f44d16b771d78e6
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-10-08 23:36:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 21.27 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,20 @@
+/**
+ * 같은 숫자는 싫어
+ * 
+ * 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지
+ * @param {Array} arr 숫자 0부터 9까지로 이루어진 배열 (1,000,000 이하의 자연수)
+ * @returns 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거한 배열
+ */
+const solution = arr => {
+    const answer = [];
+    let lastNum = NaN;
+
+    arr.forEach(n => {
+        if(lastNum !== n) {
+            answer.push(n);
+            lastNum = n;
+        }
+    });
+
+    return answer;
+}
