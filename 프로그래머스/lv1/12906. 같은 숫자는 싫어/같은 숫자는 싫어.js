5d599d42aa603c036872e59cf6c192d7d746b31a
SeungKu_Kang <kangsg26@naver.com>
2023-10-08 23:41:28 +0900

    * [Level 1] '같은 숫자는 싫어' 문제 요약
    - '다른 사람의 풀이' 코드 추가(Array::filter를 이용)
    - README.md 파일 수정

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -5,4 +5,18 @@
  * @param {Array} arr 숫자 0부터 9까지로 이루어진 배열 (1,000,000 이하의 자연수)
  * @returns 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거한 배열
  */
-const solution = arr => arr.filter((val, idx) => val != arr[idx+1]);
+const solution1 = arr => {
+    const answer = [];
+    let lastNum = NaN;
+
+    arr.forEach(n => {
+        if(lastNum !== n) {
+            answer.push(n);
+            lastNum = n;
+        }
+    });
+
+    return answer;
+}
+
+const solution2 = arr => arr.filter((val, idx) => val != arr[idx+1]);
