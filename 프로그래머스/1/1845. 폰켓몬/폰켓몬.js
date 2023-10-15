ea859f17f693b67690721aca7d14a536c8538362
KangSeungKu <53505945+KangSeungKu@users.noreply.github.com>
2023-10-15 23:57:19 +0900

    [level 1] Title: 폰켓몬, Time: 0.42 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,6 @@
+/**
+ * 
+ * @param {Array} nums N마리 폰켓몬의 종류 번호가 담긴 배열
+ * @returns N/2마리의 폰켓몬을 선택하는 방법 중, 가장 많은 종류의 폰켓몬을 선택하는 방법을 찾아, 그때의 폰켓몬 종류 번호의 개수
+ */
+const solution = nums => new Set(nums).size > nums.length / 2 ? nums.length / 2 : new Set(nums).size;
